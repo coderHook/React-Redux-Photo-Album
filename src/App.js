497a0d5f6faa545d84eee9f49a349f0044ff4d05
coderhook <abel1987@hotmail.es>
2019-06-17 11:36:50 +0200

    Redux set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +23,8 @@ function App() {
         </a>
       </header>
     </div>
+    </Provider>
+    
   );
 }
 
